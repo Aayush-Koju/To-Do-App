85831cb11866cd21b82b874e37b1e6c9806e3c98
Aayush-Koju <iush.cozu12@gmail.com>
2024-07-30 14:34:18 +0545

    implemented remove functionality

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,10 @@ function App() {
   }
   // console.log(taskList);
 
+  const remove = (index) => {
+    setTaskList(taskList.filter((_, i) => i !== index));
+  }
+
   return (
     <>
       <h1>To-Do List Application</h1>
@@ -30,13 +34,14 @@ function App() {
         <h2>Task List</h2>
         {taskList.length === 0 ?(
           <div>No Tasks Available</div>
-          ): (
-            taskList.map((task, index) =>(
-              <div key={index}>
-                <p>{index + 1}. {task}</p>
-              </div>
-          ))
-        )}
+        ): (
+          taskList.map((task, index) =>(
+            <div key={index}>
+              <p>{index + 1}. {task}</p>
+              <button onClick={() => remove(index)}>Remove</button>
+              <button>Mark as complete</button>
+            </div>
+          )))}
       </div>
     </>
   )
