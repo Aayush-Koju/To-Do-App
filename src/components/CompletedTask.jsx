bac0295f0a1e41b4cbcab9b75f8fb936ff3bc550
Aayush-Koju <iush.cozu12@gmail.com>
2024-08-09 11:11:29 +0545

    added completed task functionality

--- src/components/CompletedTask.jsx
+++ src/components/CompletedTask.jsx
@@ -1,7 +1,20 @@
-export default function CompletedTask() {
+export default function CompletedTask({ completedTasks }) {
   return (
     <>
-      <div className="completed-task">No tasks here</div>
+      <h2>Completed Tasks</h2>
+      <div className="completed-task">
+        {completedTasks.length === 0 ? (
+          <div>No completed tasks</div>
+        ) : (
+          completedTasks.map((task, index) => (
+            <div key={index}>
+              <p>
+                {index + 1}. {task}
+              </p>
+            </div>
+          ))
+        )}
+      </div>
     </>
   );
 }
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,34 +1,45 @@
 import { useState } from "react";
+import CompletedTask from "./CompletedTask";
+import "./Task.css";
 
 export default function Task({ taskList, setTaskList }) {
-  const [completedTask, setCompletedTask] = useState([]);
+  const [completedTasks, setCompletedTasks] = useState([]);
   const remove = (index) => {
     setTaskList(taskList.filter((_, i) => i !== index));
   };
 
   const complete = (index) => {
-    setCompletedTask(...completedTask, taskList[index]);
-    //remove code
+    if (!completedTasks.includes(completedTasks[index])) {
+      setCompletedTasks([...completedTasks, taskList[index]]);
+      // remove(index);
+      getClassName(index);
+    }
+    console.log(completedTasks.includes(completedTasks[index]));
   };
 
-  console.log(setCompletedTask);
+  const getClassName = (index) => {
+    return completedTasks.includes(index) ? "line" : "no-line";
+  };
 
   return (
-    <div className="task-list">
-      <h2>Task List</h2>
-      {taskList.length === 0 ? (
-        <div>No Tasks Available</div>
-      ) : (
-        taskList.map((task, index) => (
-          <div key={index}>
-            <p>
-              {index + 1}. {task}
-            </p>
-            <button onClick={() => remove(index)}>Remove</button>
-            <button onClick={() => complete(index)}>Mark as complete</button>
-          </div>
-        ))
-      )}
-    </div>
+    <>
+      <div className="task-list">
+        <h2>Task List</h2>
+        {taskList.length === 0 ? (
+          <div>No Tasks Available</div>
+        ) : (
+          taskList.map((task, index) => (
+            <div key={index}>
+              <p className={getClassName()}>
+                {index + 1}. {task}
+              </p>
+              <button onClick={() => remove(index)}>Remove</button>
+              <button onClick={() => complete(index)}>Mark as complete</button>
+            </div>
+          ))
+        )}
+      </div>
+      <CompletedTask completedTasks={completedTasks} />
+    </>
   );
 }
